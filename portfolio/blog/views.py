9f9e34093ba665a3e65dcc83192c2cf29248604e
fscliment <fscliment@protonmail.ch>
2018-10-30 07:40:13 +0100

    29102018-23_1h

--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Post
+from app_portfolio.models import Proyecto
+
+posts = Proyecto.objects.all()
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     
-    return render(request, 'post_list.html', {})
+    return render(request, 'post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(posts, pk=pk)
     return render(request, 'post_detail.html', {'post': post})
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -14,19 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import portfolio
-import app_portfolio.views
-import blog.views
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', portfolio, name = 'portfolio'),
-    path('app_portfolio/',app_portfolio, name='app_portfolio'),
-    path('blog/', blog.views.post_list, name='post_list'),
-    path('blog/post/<int:pk>/', blog.views.post_detail, name='post_detail'),
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('',include('app_portfolio.urls')),
+    path('', include('blog.urls'))
 
+    ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
