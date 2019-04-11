ebf93504e74e8d41ea6ae594ef36680cd17befca
fscliment <fscliment@protonmail.ch>
2019-04-11 20:19:35 +0200

    11042019

--- portfolio/static/static/app_portfolio/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from app_portfolio.models import *
-
-# Registre sus modelos aquí, para que aparezcan en la interfaz de administrador de Django.
-admin.site.register(Perfil)
-admin.site.register(Proyecto)
-admin.site.register(Referencia)
-admin.site.register(Experiencia)
-admin.site.register(Reconocimiento)
--- portfolio/static/static/blog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppPortfolioConfig(AppConfig):
-    name = 'blog'
